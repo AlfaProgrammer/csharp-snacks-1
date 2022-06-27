f1510b1217309849d2abdc9b7c3244b432176a48
AlfaProgrammer <alexloghin@outlook.it>
2022-06-27 15:04:01 +0200

    Snack 5

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*SNACK5*/
+/*
+   Chiedere all'utente di inserire un numero
+   Se il numero è pari stampare il numero stesso, se è dispari stampare il successivo 
+ */
 
+Console.WriteLine("Inserisci un numero: ");
+int userNumber = int.Parse(Console.ReadLine());
+
+int nToPrint;
+if (userNumber % 2 == 0)
+{
+    nToPrint = userNumber;
+}
+else {
+    nToPrint = userNumber + 1;
+}
+
+Console.WriteLine($"Il tuo numero è: {nToPrint}");
 
 /*SNACK 4*/
 int somma = 0;
@@ -18,6 +36,7 @@ Console.WriteLine($"La media dei numer è {media}");
 
 
 /*SNACK 3*/
+
 int[] numbers = new int[10];
 int sum = 0;
 
@@ -28,5 +47,4 @@ for (int i = 0; i < numbers.Length; i++) {
     sum += userNumber;
 }
 
-Console.WriteLine(sum);
-
+Console.WriteLine(sum);  
