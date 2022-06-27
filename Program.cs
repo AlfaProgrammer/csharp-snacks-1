dea5d6681619015bbac6079684716756cb04f464
AlfaProgrammer <alexloghin@outlook.it>
2022-06-27 14:54:47 +0200

    Snack 4 finito

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+/*SNACK 4*/
+int somma = 0;
+double media;
+int contatore = 0;
+for (int i = 2; i <= 10; i++)
+{
+    somma += i;
+    contatore++;
+}
+
+media = somma / contatore;
+Console.WriteLine($"La somma dei numer è {somma}");
+Console.WriteLine($"La media dei numer è {media}");
+
+
+/*SNACK 3*/
+int[] numbers = new int[10];
+int sum = 0;
+
+Console.WriteLine("Inserisci un numero: ");
+
+for (int i = 0; i < numbers.Length; i++) {
+    int userNumber = int.Parse(Console.ReadLine());
+    sum += userNumber;
+}
+
+Console.WriteLine(sum);
+
